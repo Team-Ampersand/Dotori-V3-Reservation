07259388b3ff0279883bcc13e54cc1b8091e29c7
KimGyeongsuuu <sk62628504@gmail.com>
2024-03-28 22:41:49 +0900

    project setting

---
+++ src/main/kotlin/ampersand/reservationservice/ReservationServiceApplication.kt
@@ -0,0 +1,11 @@
+package ampersand.reservationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReservationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReservationServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ampersand/reservationservice/ReservationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ampersand.reservationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
